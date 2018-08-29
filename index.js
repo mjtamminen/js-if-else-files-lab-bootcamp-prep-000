1ac102772a53f3b6180a2f8ece517a1e7e0e807b
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-08-29 08:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var name = "Joe"
 var height = "74"
-
+if (name === 12456677888+ååpoiiuuuytrrreeewwwqqasdfghjkläö'-.,mnnnbbvvvcccxxzzzz<<–……
 
 // Don't worry about this
 module.exports = { name, height
 }
-
